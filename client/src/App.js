aed4f6d720683719879313fdc25fca2a7da055be
davidkhuynh <davidhuynh726@gmail.com>
2019-03-01 14:24:06 -0800

    Progress: Home and Nav!

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
+import Home from './Home/Home';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,20 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <NavBar/>
+      <Home/>
       </div>
     );
   }
