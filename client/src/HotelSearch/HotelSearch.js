43e922940e84cdd02ad44fb34952ffecb1c2ecb3
davidkhuynh <davidhuynh726@gmail.com>
2019-03-11 15:45:28 -0700

    Added more to the first column in hotel search, and added more classes to the css

--- client/src/HotelSearch/HotelSearch.js
+++ client/src/HotelSearch/HotelSearch.js
@@ -11,32 +11,61 @@ class Home extends React.Component {
   render() {
     return (
       <div className="hotel-search-container">
+  {/* FILTER
+  	
+      		<div className="hotel-search-filter row">
+      			<div className="col-lg-12 row">
+      				<div className="col-lg-4">
+      				asd
+      				</div>
+      				<div className="col-lg-4">
+      				asd
+      				</div>
+      				<div className="col-lg-4">
+      				asd
+      				</div>
+      			</div>
+      		</div>
+
+  */}
+
+{/* HOTEL SEARCH TWO COLUMNS */}
   			<div className="hotel-search-columns row">
-	  			<div className="col-lg-5 hotel-search-first-column">
-	  				<table className="table table-bordered">
-	  				  <thead>
-	  				    <tr>
-	  				      <th scope="col">#</th>
-	  				      <th scope="col">First</th>
-	  				      <th scope="col">Last</th>
-	  				      <th scope="col">Handle</th>
-	  				    </tr>
-	  				  </thead>
+	  			<div className="col-lg-5 hotel-search-first-column table-responsive">
+	  				<table className="table hotel-search-table">
+	  				  
 	  				  <tbody>
-	  				    <tr>
+	  				    <tr className="search-hotel-row shadow-sm p-3 mb-5 bg-white rounded">
 	  				      <th scope="row">1</th>
 	  				      <td>Mark</td>
 	  				      <td>Otto</td>
 	  				      <td>@mdo</td>
 	  				    </tr>
-	  				    <tr>
+	  				    <tr className="search-hotel-row shadow-sm p-3 mb-5 bg-white rounded">
 	  				      <th scope="row">2</th>
 	  				      <td>Jacob</td>
 	  				      <td>Thornton</td>
 	  				      <td>@fat</td>
 	  				    </tr>
-	  				    <tr>
+	  				    <tr className="search-hotel-row shadow-sm p-3 mb-5 bg-white rounded">
 	  				      <th scope="row">3</th>
+	  				      <td colspan="2">Larry the Bird</td>
+	  				      <td>@twitter</td>
+	  				    </tr>
+	  				    <tr className="search-hotel-row shadow-sm p-3 mb-5 bg-white rounded">
+	  				      <th scope="row">4</th>
+	  				      <td>Mark</td>
+	  				      <td>Otto</td>
+	  				      <td>@mdo</td>
+	  				    </tr>
+	  				    <tr className="search-hotel-row shadow-sm p-3 mb-5 bg-white rounded">
+	  				      <th scope="row">5</th>
+	  				      <td>Jacob</td>
+	  				      <td>Thornton</td>
+	  				      <td>@fat</td>
+	  				    </tr>
+	  				    <tr className="search-hotel-row shadow-sm p-3 mb-5 bg-white rounded">
+	  				      <th scope="row">6</th>
 	  				      <td colspan="2">Larry the Bird</td>
 	  				      <td>@twitter</td>
 	  				    </tr>
