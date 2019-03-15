64439475654c5274aadaee32b5de749c9b687d90
kevin4coding <jiahao.liu@sjsu.edu>
2019-03-15 14:43:51 -0700

    reservation module

--- server/api.js
+++ server/api.js
@@ -45,6 +45,63 @@ router.post('/test3', (req,res)=>{
     
 })
 
+//Make a reservation 
+router.post('/reservation', (req, res)=>{
+    let response = {
+        status: 400
+    }
+    
+
+    try{
+        console("In api.js/reservation");
+        console.log("Hotel Name" + Queries.hotel);
+
+        let insertReservation = "";
+        let date_today = new Date();
+
+        let month = today_date.getUTCMonth() + 1;
+        let day = today_date.getUTCDay();
+        let year = today_date.getUTCFullYear();
+
+        insertReservation = "insert into booking (book_id, user_id, room_id, total_price, cancellation_charge, date_in, date_out, status)" +
+        "values ('"+ req.book_id +"','"+ req.user_id +"','"+ req.room_id +"','"+ req.total_price +"','"+ req.cancellation_charge +"','"+ req.date_in +"','"+ req.date_out +"','"+ req.status +"');";
+
+        console.log("booking - SQL Query " + insertReservation);
+
+        mysql.insertData(function (error, result){
+            if(error) {
+                console.log(error);
+                res(error, null);
+            }
+            else
+            {
+                console.log(result);
+                if(result.affectedRows === 1){
+                    response.status = 200;
+                    response.message = "Hotel Booked Successfully on" + month + "/" + day + "/" + year;
+                    res(null, response);
+                }
+                else
+                {
+                    response.status = 400;
+                    response.message = "Hotel Booked Failed";
+                    res(null, response);
+                }
+            }
+        }, insertReservation);
+
+    }
+    catch (e){
+        console.log(e);
+        error = e;
+        response.status = 401;
+        response.message = "Hotel Booked Failed";
+        res(error, response);
+    }
+})
+
+//validation to ensure a room cannot be double-booked
+
 
 
 module.exports = router;
