ae33fa58964b87e9ad5389e4dcc2eb0a10e73303
davidkhuynh <davidhuynh726@gmail.com>
2019-03-16 01:15:10 -0700

    Clicking Spartan Hotels redirects you to Home Clicking My Profile redirects you to UserProfile Clicking My Reservations redirects you to Reservations (Not implemented yet)
    Login, logout, registration is working fine!

--- client/src/UserProfile/UserProfile.js
+++ client/src/UserProfile/UserProfile.js
@@ -1,33 +1,90 @@
 import React from 'react';
-
-
-function UserProfile(props) {
-  return (
-
-  	<div className="home-container col-auto">
-
-	  	<div className="top-section col-auto d-flex align-content-center justify-content-center flex-wrap" >
-	  	  		
-
-		  	
-		    
+import "./../App.css";
 
 
 
-	  	</div>
-
-	  	<div className="mid-section bg-light col-auto"> 	
-	  		
-	  	</div>
-
-	  	<div className="bottom-section bg-primary col-auto"> 	
-	  	c
-	  	</div>
-
-  	</div>
-  	  );
 
 
+
+export default class UserProfile extends React.Component{
+  state = {
+
+    firstName: "",    
+    lastName: "",
+    email: "",
+    rewards: "",
+    
+  }
+  
+  change = e =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {//able to se values once submitted
+    e.preventDefault();
+    this.props.onSubmit(this.state)//possibly delete this.state
+    console.log(this.state); 
+  };
+  fileSelectedHandler = event =>{
+      console.log(event.target.files[0]);
+  }
+  
+
+  render() {
+    return(
+
+
+      <form>
+        <br/>
+        <br/>
+        <br/>
+        <br/>         
+        <br/> 
+        <h1 class = "display-4" > User Profile </h1>
+        
+        <label for="firstName" class="col-sm-1 col-form-label">First Name:</label>
+        <input 
+           name = "firstName"      
+           placeholder= 'First name' 
+           value={this.state.firstName}
+           onChange={e => this.change(e)} 
+           />      
+           <br/>
+           <br/>
+           <label for="lastName" class="col-sm-1 col-form-label">Last Name:</label>
+           <input 
+           name = "lastName"
+           placeholder= 'Last name' 
+           value={this.state.lastName} 
+           onChange={e => this.change(e)} 
+           />
+           <br/>
+           <br/>
+           <label for="email" class="col-sm-1 col-form-label">Email:</label>  
+           <input 
+           name = "email"
+           placeholder= 'Email' 
+           value={this.state.email} 
+           onChange={e => this.change(e)} 
+           />
+           <br/>           
+           <br/>
+           <button type="button" class="btn btn-outline-info" onClick = {e => this.onSubmit(e)}>UPDATE INFORMATION </button>
+           <br/>
+           <br/>
+           <h1 class = "display-4" > Rewards </h1>
+           <br/>
+           <br/>
+           <label for="rewards" ></label>  
+           <input 
+           name = "rewards"
+           placeholder= '0' 
+           value={this.state.rewards} 
+           //onChange={e => this.change(e)} 
+           />
+      </form>
+    );
+  }
 }
-
-export default UserProfile;
