46d753738ec794c77213f2f630955ebaba765725
Vincent D <lokajii@yahoo.com>
2019-04-05 23:18:47 -0700

    build and load scripts work for db v0.2

---
+++ server/sql/empty_database_v0.2.sql
@@ -0,0 +1,11 @@
+use spartanhotel;
+
+truncate table hotel;
+truncate table room;
+truncate table booking;
+truncate table hotel_image;
+truncate table room;
+truncate table room_image;
+truncate table sessions;
+truncate table rewards;
+truncate table user;
