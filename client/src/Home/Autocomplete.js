064dc36fa4359a4e2e0fe19bda9087928acec23a
Jeong Ook Moon <jeongook.moon@sjsu.edu>
2019-04-08 12:14:29 -0700

    implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page

--- client/src/Home/Autocomplete.js
+++ client/src/Home/Autocomplete.js
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 /* global google */
 
 import React from "react";
+=======
+import React from "react";
+
+>>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
 class Autocomplete extends React.Component {
   constructor(props) {
     super(props);
@@ -10,7 +15,11 @@ class Autocomplete extends React.Component {
   }
 
   componentDidMount() {
+<<<<<<< HEAD
     this.autocomplete = new google.maps.places.Autocomplete(
+=======
+    this.autocomplete = new window.google.maps.places.Autocomplete(
+>>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
       this.autocompleteInput.current,
       { types: ["geocode"] }
     );
@@ -25,13 +34,26 @@ class Autocomplete extends React.Component {
   render() {
     return (
       <input
+<<<<<<< HEAD
         ref={this.autocompleteInput}
         id="autocomplete"
         placeholder="Enter your address"
         type="text"
+=======
+        type="text"
+        className="location-input form-control"
+        ref={this.autocompleteInput}
+        id="search_bar"
+        placeholder="Where are you going?"
+
+>>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
       />
     );
   }
 }
 
 export default Autocomplete;
+<<<<<<< HEAD
+=======
+
+>>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
--- client/src/HotelSearchDemo/HotelSearchDemo.js
+++ client/src/HotelSearchDemo/HotelSearchDemo.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
 import { withRouter } from 'react-router-dom'
 import homeImage from './Images/homeImage7.jpg';
 
 import axios from 'axios'
+<<<<<<< HEAD
 import { HotelSearchFunction } from '../Utility/HotelSearchFunction'
 
 
@@ -20,12 +20,13 @@ const mapStyles = {
 	width: '100%',
 	height: '100%'
 };
+=======
+>>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
 
 class HotelSearchDemo extends React.Component {
 
 	constructor(props) {
 		super(props);
-		const { data } = this.props.location;
 		this.state = {
 			hotels: [{}],
 			address: "",
@@ -56,12 +57,58 @@ class HotelSearchDemo extends React.Component {
 		const hotelSearch = (await axios.get(queryCall)).data;
 		this.setState({
 			hotels: hotelSearch
-		});
-		console.log("hotelSearch");
-		console.log(hotelSearch);
-		console.log(this.state.hotels.results.length);
+		}, this.renderMap());
 	}
 
+	initializeMap = () => {
+		let geocoder = new window.google.maps.Geocoder();
+
+		let googleMap = new window.google.maps.Map(document.getElementById('map'), {
+			center: { lat: 0, lng: 0 },
+			zoom: 11
+		});
+
+		let params = new URLSearchParams(this.props.location.search);
+		let city_name = params.get('city')
+
+		// display the center of the map by city name
+		geocoder.geocode({ 'address': city_name }, function (results, status) {
+			if (status === 'OK') {
+				googleMap.setCenter(results[0].geometry.location);
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
+		});
+
+		// display each hotel's information window when clicking the marker	
+		let infoWindow = new window.google.maps.InfoWindow()
+
+		this.state.hotels.results.map((eachHotel, index) => {
+
+			let hotelInfo = `<h6>${eachHotel.name}</h6>
+											 <p>${eachHotel.address}</p>`
+
+			// display each hotel's marker along with index number
+			let marker = new window.google.maps.Marker({
+				position: { lat: parseFloat(eachHotel.latitude), lng: parseFloat(eachHotel.longitude) },
+				map: googleMap,
+				label: (index + 1).toString(),
+				title: eachHotel.name
+			})
+
+			// action listener to open information window when clicking marker
+			marker.addListener('click', function () {
+				infoWindow.setContent(hotelInfo)
+				infoWindow.open(googleMap, marker);
+			});
+		})
+	}
+
+	renderMap() {
+		const CALLBACK_URL = "https://maps.googleapis.com/maps/api/js?key="+process.env.REACT_APP_GOOGLE_MAP_API_KEY+"&callback=initMap" 
+		loadGoogleMapScript(CALLBACK_URL)
+		window.initMap = this.initializeMap
+	}
 
 
 	roomSearch = item => event => {
@@ -94,7 +141,7 @@ class HotelSearchDemo extends React.Component {
 		console.log("status number(200 success, else fail): ")
 		console.log("expected reponse 200  ")
 		//we don't have a query that handles the room
-		let queryString = this.props.location.search+"&zip="+item.zipcode
+		let queryString = this.props.location.search + "&zip=" + item.zipcode
 		this.props.history.push({
 			pathname: `/RoomPage`,
 			search: `${queryString}`,
@@ -116,13 +163,7 @@ class HotelSearchDemo extends React.Component {
 			state: item.state,
 			zipcode: item.zipcode,
 		})
-
-
 	}
-
-
-
-
 
 	render() {
 		console.log(this.props.location.search)
@@ -150,76 +191,7 @@ class HotelSearchDemo extends React.Component {
 			< div className="hotel-search-columns hotel-search-item-row">
 
 				<div className="col-lg-4 hotel-demo-dummy-column ">
-					<div className="col-lg-12 hotel-search-table-container-demo">
-						<div className="hotel-search-first-column-demo col-lg-12 shadow-sm">
-
-							<h1 className="hotel-search-header-demo">Search By</h1>
-							<hr></hr>
-							<div className="col-lg-12">
-
-								<div className="hotel-search-location-header-container-demo">
-									<div className="hotel-search-location-header-demo">
-										Location:
-										</div>
-
-									<div className="hotel-search-location-demo">
-										{toTitleCase(location)}
-									</div>
-								</div>
-
-								<div className="row hotel-search-date-container-demo">
-
-									<div className="col-lg-6">
-										<div className="hotel-search-date-in-header-demo">
-											Date In:
-											</div>
-
-										<div className="hotel-search-date-in-demo">
-											{dateIn}
-										</div>
-									</div>
-
-									<div className="col-lg-6">
-										<div className="hotel-search-date-out-header-demo">
-											Date Out:
-											</div>
-
-										<div className="hotel-search-date-out-demo">
-											{dateOut}
-										</div>
-									</div>
-
-								</div>
-
-								<div className="row hotel-search-guest-container-demo">
-
-									<div className="col-lg-6">
-										<div className="hotel-search-date-in-header-demo">
-											Adults #:
-											</div>
-
-										<div className="hotel-search-date-in-demo">
-											{adult}
-										</div>
-									</div>
-
-									<div className="col-lg-6">
-										<div className="hotel-search-date-out-header-demo">
-											Children #:
-											</div>
-
-										<div className="hotel-search-date-out-demo">
-											{children}
-										</div>
-									</div>
-
-								</div>
-
-
-
-							</div>
-						</div>
-					</div>
+					<div id="map"></div>
 				</div>
 
 				{/* HOTEL SEARCH TWO COLUMNS, Second Column */}
@@ -251,7 +223,7 @@ class HotelSearchDemo extends React.Component {
 														<div className="col-lg-1 hotel-search-item-number">{index + 1}</div>
 														<div className="">.</div>
 														{/* Hotel Name */}
-														<a href="#" className="col-lg-10 hotel-search-item-name">{eachHotelResult.name}</a>
+														<a href='' onClick={this.roomSearch(eachHotelResult)} value={eachHotelResult} className="col-lg-10 hotel-search-item-name">{eachHotelResult.name}</a>
 													</div>
 
 													<div className="hotel-search-item-row hotel-search-item-rating">
@@ -393,17 +365,21 @@ class HotelSearchDemo extends React.Component {
 		if (this.state.hotels.results.length === 0) {
 			return (
 <<<<<<< HEAD
+<<<<<<< HEAD
 			<div className="" style={topSectionStyle}>
 			<div className="hotel-search-container"> {showNoResult} </div>
 			</div>
 =======
 
+=======
+>>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
 				<div className="hotel-search-container"> {showNoResult} </div>
 >>>>>>> room page retrieve data using query url
 			);
 		}
 
 		else {
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 		return (
@@ -417,10 +393,10 @@ class HotelSearchDemo extends React.Component {
 			</div>
 		);
 =======
+=======
+>>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
 			return (
 				<div className="hotel-search-container">
-
-
 					{showResult}
 				</div>
 			);
@@ -429,6 +405,15 @@ class HotelSearchDemo extends React.Component {
 		}
 	}
 
+}
+
+function loadGoogleMapScript(src) {
+	let index = window.document.getElementsByTagName("script")[0]
+	let script = window.document.createElement("script")
+	script.src = src
+	script.async = true
+	script.defer = true
+	index.parentNode.insertBefore(script, index) // insert google map script before any script in index html
 }
 
 export default withRouter(HotelSearchDemo);
