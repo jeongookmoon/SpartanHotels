98377462faf5c3e02e52aa74bc029087d039f1d2
Jeong Ook Moon <jeongook.moon@sjsu.edu>
2019-04-08 18:43:56 -0700

    rebased base off new ui

--- client/src/Home/Autocomplete.js
+++ client/src/Home/Autocomplete.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-/* global google */
-
-import React from "react";
-=======
 import React from "react";
 
->>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
 class Autocomplete extends React.Component {
   constructor(props) {
     super(props);
@@ -15,11 +9,7 @@ class Autocomplete extends React.Component {
   }
 
   componentDidMount() {
-<<<<<<< HEAD
-    this.autocomplete = new google.maps.places.Autocomplete(
-=======
     this.autocomplete = new window.google.maps.places.Autocomplete(
->>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
       this.autocompleteInput.current,
       { types: ["geocode"] }
     );
@@ -34,26 +24,15 @@ class Autocomplete extends React.Component {
   render() {
     return (
       <input
-<<<<<<< HEAD
-        ref={this.autocompleteInput}
-        id="autocomplete"
-        placeholder="Enter your address"
-        type="text"
-=======
         type="text"
         className="location-input form-control"
         ref={this.autocompleteInput}
         id="search_bar"
         placeholder="Where are you going?"
 
->>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
       />
     );
   }
 }
 
 export default Autocomplete;
-<<<<<<< HEAD
-=======
-
->>>>>>> implemented 3 features: autocomplete for address search bar, airbnb style datepicker and google map on search result page
