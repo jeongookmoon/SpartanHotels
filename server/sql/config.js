80b7aad14bf70c1bd0af35252d16785ab392ca48
LetTheBassKick <royzhang42@gmail.com>
2019-04-18 15:54:33 -0700

    Added status codes for responses on everything.

--- server/sql/config.js
+++
@@ -1,8 +0,0 @@
-let config = {
-    host:'localhost',
-    user:'cmpe165',
-    password:'hotel165',
-    database:'spartanhotel'
-}
-
-module.exports = config
--- server/sql/empty_database_v0.sql
+++
@@ -1,6 +0,0 @@
-use spartanhotel;
-
-truncate table hotel;
-truncate table room;
-truncate table booking;
-truncate table hotel_image;
