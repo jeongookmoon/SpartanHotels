5fc428e6bd5f758b36e0d7cf9730c731e10a4aa6
kevin4coding <jiahao.liu@sjsu.edu>
2019-04-19 15:29:48 -0700

    functions dev done

--- client/src/Recoverage/Accesscode.js
+++ client/src/Recoverage/Accesscode.js
@@ -40,7 +40,7 @@ class Accesscode extends Component {
             {req:"At least 1 lowercase letter", valid:false},
             {req:"At least 1 special character !@#$%^&*",valid:false}]
         };
-        this.passwordChecker = this.passwordChecker.bind(this);
+        // this.passwordChecker = this.passwordChecker.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
@@ -57,39 +57,39 @@ class Accesscode extends Component {
           )
      }
 
-    passwordChecker(){
-        let pw = this.state.fields.password;
-        let tmp_passwordCheck = this.state.passwordCheck
+    // passwordChecker(){
+    //     let pw = this.state.fields.password;
+    //     let tmp_passwordCheck = this.state.passwordCheck
     
-        // req:"≥ 8 characters"
-        tmp_passwordCheck[0].valid = (pw.length >= 8) ? true : false
+    //     // req:"≥ 8 characters"
+    //     tmp_passwordCheck[0].valid = (pw.length >= 8) ? true : false
         
-        // At least 1 Uppercase letter
-        if( /(?=.*[A-Z])/.test(pw)){
-          tmp_passwordCheck[1].valid = true
-        }
-        else{
-          tmp_passwordCheck[1].valid = false
-        }
+    //     // At least 1 Uppercase letter
+    //     if( /(?=.*[A-Z])/.test(pw)){
+    //       tmp_passwordCheck[1].valid = true
+    //     }
+    //     else{
+    //       tmp_passwordCheck[1].valid = false
+    //     }
     
-        // At least 1 Lowercase letter
-        if( /(?=.*[a-z])/.test(pw)){
-          tmp_passwordCheck[2].valid = true
-        }
-        else{
-          tmp_passwordCheck[2].valid = false
-        }
+    //     // At least 1 Lowercase letter
+    //     if( /(?=.*[a-z])/.test(pw)){
+    //       tmp_passwordCheck[2].valid = true
+    //     }
+    //     else{
+    //       tmp_passwordCheck[2].valid = false
+    //     }
     
-        // At least 1 special character !@#$%^&*
-        if( /(?=.*[!@#$%^&*])/.test(pw)){
-          tmp_passwordCheck[3].valid = true
-        }
-        else{
-          tmp_passwordCheck[3].valid = false
-        }
+    //     // At least 1 special character !@#$%^&*
+    //     if( /(?=.*[!@#$%^&*])/.test(pw)){
+    //       tmp_passwordCheck[3].valid = true
+    //     }
+    //     else{
+    //       tmp_passwordCheck[3].valid = false
+    //     }
     
-        this.setState({ passwordCheck: tmp_passwordCheck});
-      }
+    //     this.setState({ passwordCheck: tmp_passwordCheck});
+    //   }
 
     checkCode = (e) => {
       e.preventDefault();
