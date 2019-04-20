916e6b6e8b7426397a5acef264fb5846b37de87c
kevin4coding <jiahao.liu@sjsu.edu>
2019-04-19 21:41:13 -0700

    remind msg done, wait for background

--- client/src/Recoverage/Recoverage.js
+++ client/src/Recoverage/Recoverage.js
@@ -5,16 +5,26 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { sendcodePost } from '../Utility/RecoverageFunction';
 
+    
+    // var topSectionStyle = {
+    //     width: "100%",
+    //     height: '450px',
+    //     marginTop: "0%",
+    //     backgroundRepeat: "no-repeat",
+    //     backgroundSize: "cover",
+    //     backgroundPosition: "center center",
+    //     backgroundImage: `url(${recoveryImage})`
+    // };
+       let card ={
+        width: '275px'
+       };
+       let container = {
+        marginLeft: "45%"
+       }
+       var topSectionStyle = {
+        marginTop:"10%",
+      };
 
-let card ={
-    width: '275px'
-   };
-   let container = {
-    marginLeft: "45%"
-   }
-//    var topSectionStyle = {
-//     marginTop:"10%",
-//   };
 
 class Recoverage extends Component {
     constructor(props) {
@@ -71,88 +81,57 @@ class Recoverage extends Component {
                     msgFromServer: '',
                     showNullError: false,
                 })
-            }
-        })
-        
-
-    }
-    // else {
-    //     axios
-    //     .post('http://localhost:3000/recovery', {
-    //         email,
-    //     })
-    //     .then((repsonse) => {
-    //         console.log(response.data);
-    //         if(response.data === 'recovery email sent!') {
-    //             this.setState({
-    //                 showError: false,
-    //                 msgFromServer: 'recovery email sent!',
-    //                 showNullError: false,
-    //             });
-    //         }
-    //     })
-    //     .catch((error) => {
-    //         console.error(error.response.data);
-    //         if(error.response.data === 'email not in db'){
-    //             this.setState({
-    //                 showError: true,
-    //                 msgFromServer: '',
-    //                 showNullError: false,
-    //             });
-    //         }
-    //     });
-    // }
-    
+                }
+            })
         }
+    }
 
     
     render(){
         const {
             email, msgFromServer, showNullError, showError
         } = this.state;
-        var topSectionStyle = {
-    		marginTop:"6.5em",
-		};
 
 		// var midSectionStyle = {
 		// 	marginTop: "2em",
 		// };
         return(
-            <div style={container}>
-            <div className="topheader" style={topSectionStyle}>
-              <Card style={card} >
-                <CardContent>
-                  <div className="col-auto pl-0">
-					<h3> Password Recoverage </h3>
-				  </div>
-                    <TextField
-                    //   style={inputStyle}
-                      id="email"
-                      label="email"
-                      value={email}
-                      onChange={this.handleChange('email')}
-                      placeholder="email address"     
-                    />
-                    <button type="submit" color="primary" onClick={this.sendcode}>
-                    Send Code</button>
-                    {showNullError && (
-                        <div>
-                            <p>The email address cannot be null.</p>
+            <div style={topSectionStyle}>
+                <div className="topheader" style={container}>
+                    <Card style={card} >
+                        <CardContent>
+                        <div className="col-auto pl-0">
+                            <h3> Password Recoverage </h3>
                         </div>
-                    )}
-                    {showError && (
-                        <div>
-                            <p>The email address has not been register!</p>
-                        </div>
-                    )}
-                    {msgFromServer === 'recovery email sent!' && (
-                        <div>
-                            <p>Access code has sent!</p>
-                        </div>
-                    )}
-                    </CardContent>
-                </Card>
-            </div>
+                            <TextField
+                            //   style={inputStyle}
+                            id="email"
+                            label="email"
+                            type="email"
+                            value={email}
+                            onChange={this.handleChange('email')}
+                            placeholder="email address"     
+                            />
+                            <button type="submit" color="primary" onClick={this.sendcode}>
+                            Send Code</button>
+                            {showNullError && (
+                                <div>
+                                    <p>The email address cannot be null.</p>
+                                </div>
+                            )}
+                            {showError && (
+                                <div>
+                                    <p>The email address has not been register!</p>
+                                </div>
+                            )}
+                            {msgFromServer === 'recovery email sent!' && (
+                                <div>
+                                    <p>Access code has sent!</p>
+                                </div>
+                            )}
+                            </CardContent>
+                    </Card>
+                </div>
         </div>
         )
     } 
