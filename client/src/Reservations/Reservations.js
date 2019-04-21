c325f5849606fc14c119e34885367e19bfaf2533
sophiechenn <sophiechenus@yahoo.com>
2019-04-20 20:16:24 -0700

    update profile and view reservations

--- client/src/Reservations/Reservations.js
+++ client/src/Reservations/Reservations.js
@@ -1,71 +1,137 @@
-import React, {Component} from 'react';
-//import './ReservationsStyle.css';
+import React from 'react';
+import { Table, Button, Card, 
+		CardBody, CardTitle, CardHeader,
+		Container, Row, Col } from 'reactstrap';
+import homeImage from './homeImage.jpg';
+import "./Reservations.css";
 
+var pageStyle = {
+	width: "100%",
+	backgroundRepeat: "no-repeat",
+	backgroundSize: "cover",
+	backgroundPosition: "center center",
+	backgroundImage: `url(${homeImage})`,
 
-class Reservations extends Component {
+};
+
+class Reservations extends React.Component {
 	constructor (props) {
 		super (props)
 		this.state = {
 			history: []
 		}
-		//this.loadData = this.loadData.bind(this);
-	}
-
-	componentDidMount() {
-		//this.loadData()
-		let self = this;
-		fetch('/history', {
-			method: 'GET'
-		}).then(function(response) {
-			if (response.status >= 400) {
-				throw new Error ("Bad response from server");
-			}
-			return response.json();
-		}).then(function(data) {
-			self.setState({history: data});
-		}).catch(err => {
-			console.log('caught it!', err);
-		})
 	}
 
 	render() {
-		var topSectionStyle = {
-    		marginTop:"6.5em",
-		};
-
-		var midSectionStyle = {
-			marginTop: "2em",
-		};
 		return (
-			<div className="topheader" style={topSectionStyle}>
-				<div className="col-auto pl-0">
-					<h3> MY RESERVATIONS </h3>
-				</div>
-				<div class="container" style={midSectionStyle}>
-					<table class="table table-bordered">
-	    				<thead>
-	      					<tr>
-	       						<th>Booking ID</th>
-	        					<th>Date In</th>
-	        					<th>Date Out</th>
-	        					<th>Hotel</th>
-	        					<th>Room</th>
-	        					<th>Total Price</th>
-	        					<th>Edit/Cancel</th>
-	        					<th>Status</th>
-	      					</tr>
-	    				</thead>
-	    				<tbody>
-				     		<td> N/A </td>
-				     		<td> N/A </td>
-				        	<td> N/A </td>
-				        	<td> N/A </td>
-				        	<td> N/A </td>
-				        	<td> N/A </td>
-				        	<td> </td>
-				        	<td> N/A </td>
-	    				</tbody>
-	  			</table>
+			<div className="col-lg-12 reservations-container col-auto" style={pageStyle}>
+				<div className="reservations-form-container col-lg-12">
+					<br/>
+					<br/>
+					<br/>
+					<br/>         
+					<br/> 
+					<div>
+						<Container>
+							<Row>
+								<Col>
+									<Card>
+										<CardBody>
+											<br/>
+											<CardTitle> <h2> My Reservations </h2> </CardTitle>
+											<br />
+											<div className="reservations-table-wrapper-scroll-y reservations-scrollbar">
+												<Table hover>
+								    				<thead>
+								      					<tr>
+								       						<th className="reservations-table-header">Booking ID</th>
+								        					<th className="reservations-table-header">Date In</th>
+								        					<th className="reservations-table-header">Date Out</th>
+								        					<th className="reservations-table-header">Hotel</th>
+								        					<th className="reservations-table-header">Room Type, Quantity</th>
+								        					<th className="reservations-table-header">Total Price</th>
+								        					<th className="reservations-table-header">Modify/Cancel</th>
+								        					<th className="reservations-table-header">Status</th>
+								      					</tr>
+								    				</thead>
+								    				<tbody>
+								    					<tr>
+													     		<td> holder </td>
+													     		<td> holder </td>
+													        	<td> holder </td>
+													        	<td> 
+													        		holder
+													        		<br />
+													        		include address
+													        	</td>
+													        	<td> 
+													        		<Table size="sm" borderless>
+													        			holder, 3
+													        			<br />
+																		can also display room #
+													        		</Table>
+													        	</td>
+													        	<td> holder </td>
+													        	<td> <Button color ="warning"> Modify </Button> <Button color="danger"> Cancel </Button> </td>
+													        	<td> holder </td>
+											        	</tr>
+								    					<tr>
+													     		<td> holder </td>
+													     		<td> holder </td>
+													        	<td> holder </td>
+													        	<td> holder </td>
+													        	<td> 
+													        		<Table size="sm" borderless>
+													        			holder, 3
+													        			<br />
+																		holder, 2
+													        		</Table>
+													        	</td>
+													        	<td> holder </td>
+													        	<td> <Button color ="warning"> Modify </Button> <Button color="danger"> Cancel </Button> </td>
+													        	<td> holder </td>
+											        	</tr>
+								    					<tr>
+													     		<td> holder </td>
+													     		<td> holder </td>
+													        	<td> holder </td>
+													        	<td> holder </td>
+													        	<td> 
+													        		<Table size="sm" borderless>
+													        			holder, 3
+													        			<br />
+																		holder, 2
+													        		</Table>
+													        	</td>
+													        	<td> holder </td>
+													        	<td> <Button color ="warning"> Modify </Button> <Button color="danger"> Cancel </Button> </td>
+													        	<td> holder </td>
+											        	</tr>
+								    					<tr>
+													     		<td> holder </td>
+													     		<td> holder </td>
+													        	<td> holder </td>
+													        	<td> holder </td>
+													        	<td> 
+													        		<Table size="sm" borderless>
+													        			holder, 3
+													        			<br />
+																		holder, 2
+													        		</Table>
+													        	</td>
+													        	<td> holder </td>
+													        	<td> <Button color ="warning"> Modify </Button> <Button color="danger"> Cancel </Button> </td>
+													        	<td> holder </td>
+											        	</tr>
+								    				</tbody>
+								    			</Table>
+							    			</div>
+								    	</CardBody>
+								    </Card>
+								</Col>
+							</Row>
+						</Container>
+					</div>
 				</div>
 			</div>
 		);
---
+++ client/src/UserProfile/ProfileEditPassword.js
@@ -0,0 +1,186 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom'
+import axios from 'axios';
+
+import {
+  Button, Modal, ModalHeader, ModalBody, ModalFooter,
+  Form, FormGroup, Label, Input, Row, Col,
+  UncontrolledPopover, PopoverHeader, PopoverBody,
+} from 'reactstrap'
+
+class ProfileEditPassword extends React.Component {
+	constructor() {
+		super();
+
+		this.state={
+			modal:false,
+			fields: {
+				oldpass: '',
+				newpass: '',
+				repass: '',
+			},
+			errors: {
+			},
+			password_error: [],
+			email_duplicate_error: false,
+
+			passwordCheck: [{req:"≥ 8 characters", valid:false},
+      		{req:"At least 1 uppercase letter", valid:false},
+		    {req:"At least 1 lowercase letter", valid:false},
+		    {req:"At least 1 special character !@#$%^&*",valid:false}]
+		};
+
+		this.toggle = this.toggle.bind(this);
+		this.updateFields = this.updateFields.bind(this);
+		this.oldPasswordChecker = this.oldPasswordChecker.bind(this);
+		this.newPasswordChecker = this.newPasswordChecker.bind(this);
+	}
+
+	updateFields(event) {
+		let temp_fields = this.state.fields;
+		temp_fields[event.target.name] = event.target.value;
+		this.setState({ fields : temp_fields });
+		this.newPasswordChecker()
+	}
+
+	oldPasswordChecker() {
+		
+	}
+
+	newPasswordChecker () {
+		let pw = this.state.fields.newpass;
+		let tmp_passwordCheck = this.state.newPasswordCheck
+
+			tmp_passwordCheck[0].valid = (pw.length >= 8) ? true : false
+    
+	    // At least 1 Uppercase letter
+	    if( /(?=.*[A-Z])/.test(pw)){
+	      tmp_passwordCheck[1].valid = true
+	    }
+	    else{
+	      tmp_passwordCheck[1].valid = false
+	    }
+
+	    // At least 1 Lowercase letter
+	    if( /(?=.*[a-z])/.test(pw)){
+	      tmp_passwordCheck[2].valid = true
+	    }
+	    else{
+	      tmp_passwordCheck[2].valid = false
+	    }
+
+	    // At least 1 special character !@#$%^&*
+	    if( /(?=.*[!@#$%^&*])/.test(pw)){
+	      tmp_passwordCheck[3].valid = true
+	    }
+	    else{
+	      tmp_passwordCheck[3].valid = false
+	    }
+
+	    this.setState({ passwordCheck: tmp_passwordCheck});
+	}
+
+	validate() {
+	    let temp_fields = this.state.fields;
+	    let temp_errors = {};
+	    let temp_password_error = [];
+	    let formIsValid = true;
+
+	    if (temp_fields["newpass"] === '') {
+	      formIsValid = false;
+	      temp_errors["newpass"] = "*Please enter a password";
+	    }
+
+	    if (temp_fields["newpass"] !== '') {
+	      let checker = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$");
+	      if (!checker.test(temp_fields["password"])) {
+	        formIsValid = false;
+	        temp_errors["newpass"] = "*This password does not meet the requirements"
+	      }
+
+	      else{
+	        if (temp_fields["repass"] === '') {
+	          formIsValid = false;
+	          temp_errors["repass"] = "*Please re-enter password";
+	        }
+	    
+	        if (temp_fields["repass"] !== '') {
+	          if (!temp_fields["repass"].match(temp_fields["newpass"])) {
+	            formIsValid = false;
+	            temp_errors["repass"] = "*Passwords must match";
+	          }
+	        }
+	      }
+	    }
+
+	    this.setState({
+	      errors: temp_errors,
+	      password_error : temp_password_error
+	    });
+	    return formIsValid;
+	}
+
+	// need something to get old password
+	componentDidMount() {
+	    axios.get('/api/profile')
+	        .then(res => 
+	          this.setState({
+	            name: res.data[0].name,
+	            email: res.data[0].email,
+	        }))  
+	}
+
+	toggle() {
+		this.setState({
+			...this.state,
+			modal: !this.state.modal
+		})
+	}
+
+	render() {
+
+		const password_error = (
+      		<div className="text-warning">{this.state.password_error.map((each) => <div>{each}</div>
+      	)}</div>
+    	)
+
+	    const no_error = (
+	      <div className="text-warning"></div>
+	    )
+
+		return (
+			<div>
+	        	<Button size="sm" onClick={this.toggle} color="info">Change Password</Button>
+
+				<Modal isOpen={this.state.modal} toggle={this.toggle} centered>
+					<ModalHeader toggle={this.toggle}> Change Your Password </ModalHeader>
+
+					<ModalBody>
+						<Form onSubmit={this.handleSubmit}>
+							<FormGroup>
+								<Label> Old Password: </Label>
+								<Input type="text" name="oldpass" placeholder="********" onChange={this.handleUpdate} required />
+							</FormGroup>
+							<FormGroup>
+								<Label> New Password: </Label>
+								<Input type="text" name="newpass" placeholder="********" onChange={this.handleUpdate} required />
+							</FormGroup>
+							<FormGroup>
+								<Label> Re-enter New Password: </Label>
+								<Input type="text" name="repass" placeholder="********" onChange={this.handleUpdate} required />
+							</FormGroup>
+						</Form>
+					</ModalBody>
+
+					<ModalFooter>
+						<Button color="primary" onClick={this.handleSubmit}> Save </Button>
+						<Button color="secondary" onClick={this.toggle}> Cancel </Button>
+					</ModalFooter>
+
+				</Modal>
+			</div>
+		)
+	}
+}
+
+export default withRouter(ProfileEditPassword);
