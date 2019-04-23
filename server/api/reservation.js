d84a800eb666e8d86b30917e71af56f91184ff72
Vincent D <lokajii@yahoo.com>
2019-04-23 02:34:33 -0700

    refactor getUserEmail

--- server/api/reservation.js
+++ server/api/reservation.js
@@ -211,25 +211,27 @@ router.post('/', (req, res)=>{
 
 
 
-    async function getUserEmail(userid) {
-        let emailquery = mysql.format('SELECT email FROM user WHERE user_id = ?', userid)
-        let userEmail;
-        try{
-            userEmail = await Queries.run(emailquery).then(function(results) {
-                console.log('email for user is' + results[0].email)
-                return results[0].email
-            })
-        } catch(e){
-            // query failed for some reason
-            console.log(e)
-            res.status(500).send("email service failure")
-            return
-        }
-        return userEmail
-    }
+
 
     
 })
+
+async function getUserEmail(userid) {
+    let emailquery = mysql.format('SELECT email FROM user WHERE user_id = ?', userid)
+    let userEmail;
+    try{
+        userEmail = await Queries.run(emailquery).then(function(results) {
+            console.log('email for user is' + results[0].email)
+            return results[0].email
+        })
+    } catch(e){
+        // query failed for some reason
+        console.log(e)
+        res.status(500).send("email service failure")
+        return
+    }
+    return userEmail
+}
 
 router.get('/viewres', (req, res) => {
     if (typeof(req.user) == 'undefined') {
