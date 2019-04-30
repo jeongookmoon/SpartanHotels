19afad39f0757ba3c80f2871cff3fcbcd0989580
LetTheBassKick <royzhang42@gmail.com>
2019-04-29 17:29:49 -0700

    Aded info for bed type, room price, and quantity

--- client/src/Reservations/Reservations.js
+++ client/src/Reservations/Reservations.js
@@ -32,20 +32,58 @@ class Reservations extends React.Component {
       .then(function(viewres) {
       	var resInfo = []
       	var room_info = []
-      	var room_numbers = []
-      	var room = 0
+      	var bed_types = []
+      	var room_prices = []
+      	var quantities = []
       	for (var i = 0; i < viewres.data.length; i++) {
-      		room_numbers.push(viewres.data[i].room_number)
+      		bed_types.push(viewres.data[i].bed_type)
+      		room_prices.push(viewres.data[i].price)
+      		quantities.push(1)
+      		for (var j = i + 1; j < viewres.data.length; j++) {
+      			if (viewres.data[j].transaction_id == viewres.data[i].transaction_id) {
+      				bed_types.push(viewres.data[j].bed_type)
+      				room_prices.push(viewres.data[j].price)
+      				quantities.push(1)
+      			}
+      			else {
+      				i = j - 1
+      				break
+      			}
+      		}
+      		room_info.push({"beds": bed_types, "prices": room_prices, "quans": quantities})
+      		bed_types = []
+      		room_prices = []
+      		quantities = []
       	}
-      	for (var x = 0; x < viewres.data.length; x++) {
-      		var booking_id = viewres.data[x].transaction_id
-      		var date_in = viewres.data[x].date_in
-      		var date_out = viewres.data[x].date_out
-      		var hotel_name = viewres.data[x].name
-      		var total_price = viewres.data[x].total_price
-      		var status = viewres.data[x].status
-      		
-      		resInfo[x] = {booking_id, date_in, date_out, hotel_name, room_info, total_price, status}
+      	console.log(room_info)
+      		var booking_id = viewres.data[0].transaction_id
+      		var date_in = viewres.data[0].date_in
+      		var date_out = viewres.data[0].date_out
+      		var hotel_name = viewres.data[0].name
+      		var bed_type = room_info[0].beds.toString()
+      		var room_price = room_info[0].prices.toString()
+      		var room_quantities = room_info[0].quans.toString()
+      		var total_price = viewres.data[0].total_price
+      		var status = viewres.data[0].status
+
+      		resInfo[0] = {booking_id, date_in, date_out, hotel_name, bed_type, room_price, room_quantities, total_price, status}
+      		var counter = 1
+      	for (var x = 1; x < viewres.data.length; x++) {
+      		if (viewres.data[x].transaction_id == viewres.data[x - 1].transaction_id) {
+      			continue
+      		}
+      		booking_id = viewres.data[x].transaction_id
+      		date_in = viewres.data[x].date_in
+      		date_out = viewres.data[x].date_out
+      		hotel_name = viewres.data[x].name
+      		bed_type = room_info[counter].beds.toString()
+      		room_price = room_info[counter].prices.toString()
+      		room_quantities = room_info[counter].quans.toString()
+      		total_price = viewres.data[x].total_price
+      		status = viewres.data[x].status
+      		counter++
+
+      		resInfo[x] = {booking_id, date_in, date_out, hotel_name, bed_type, room_price, room_quantities, total_price, status}
       	}
       	console.log(resInfo)
       	that.setState({
@@ -56,14 +94,16 @@ class Reservations extends React.Component {
 
   	renderReservationsTableData() {
 		return this.state.history.map((reservations, index) => {
-			const { booking_id, date_in, date_out, hotel_name, room_info, total_price, status } = reservations //destructuring
+			const {booking_id, date_in, date_out, hotel_name, bed_type, room_price, room_quantities, total_price, status} = reservations //destructuring
 				return (
 				<tr >
 					<td>{booking_id}</td>
 					<td>{date_in}</td>
 					<td>{date_out}</td>
 					<td>{hotel_name}</td>
-					<td>{room_info}</td>
+					<td>{bed_type}</td>
+					<td>{room_price}</td>
+					<td>{room_quantities}</td>
 					<td>{total_price}</td>
 					<td><Button color ="warning"> Modify </Button> <Button color="danger"> Cancel </Button></td>
 					<td>{status}</td>
@@ -101,7 +141,9 @@ class Reservations extends React.Component {
 														<th className="reservations-table-header">Date In</th>
 														<th className="reservations-table-header">Date Out</th>
 														<th className="reservations-table-header">Hotel</th>
-														<th className="reservations-table-header">Room Type, Quantity</th>
+														<th className="reservations-table-header">Bed Type</th>
+														<th className="reservations-table-header">Room Price</th>
+														<th className="reservations-table-header">Quantity</th>
 														<th className="reservations-table-header">Total Price</th>
 														<th className="reservations-table-header">Modify/Cancel</th>
 														<th className="reservations-table-header">Status</th>
@@ -125,7 +167,6 @@ class Reservations extends React.Component {
 			<div>
 				<div className="reservations-container">
 					{localStorage.accesstoken ? reservationPage : this.redirectToHome()}
-					{reservationPage}
 				</div>
 			</div>
 		);
