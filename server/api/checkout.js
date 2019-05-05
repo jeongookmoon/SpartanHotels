0669f33171aa32ab6580728959a115dc0b4e03a7
Vincent D <lokajii@yahoo.com>
2019-05-04 21:05:22 -0700

    checkout charge being made to stripe api

--- server/api/checkout.js
+++ server/api/checkout.js
@@ -7,13 +7,46 @@ const bodyParser  = require('body-parser');
 const stripe = require("stripe")("sk_test_KMjoJvcxhuiJSV51GJcaJfSi00r9QtVXjo"); // Your Stripe key
 
 
-
+router.use(bodyParser.text());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', (req, res) => {
-    router.use(bodyParser.text());
-    router.use(bodyParser.urlencoded({ extended: true }));
     res.status(200).send("hello ")
 })
+
+
+
+router.post("/charge", async (req, res) => {
+    console.log(req.body)
+
+    try {
+      var data = JSON.parse(req.body)
+  
+      let status = await stripe.charges.create({
+        amount: data.total_price*100,
+        currency: "usd",
+        description: "Charge",
+        source: data.id,
+      });
+  
+     console.log(status.id); // retrieves the charge
+  
+  
+     
+      console.log(status);
+      //console.log("amount: "+data.amount);
+      console.log(data);
+  
+      res.json({status});
+  
+    } catch (err) {
+      console.log(err);
+      res.status(500).end();
+    }
+  
+  
+  });
+  
 
 
 module.exports = router;
